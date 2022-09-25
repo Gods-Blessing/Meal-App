0bdd95efde8b58c98c4c369e6fbed5c474b15194
mohit <mohitadhana2569@gmail.com>
2022-09-25 23:04:55 +0530

    added favourite button

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 let imgs = document.getElementById('imgs');
 
-let btn = document.getElementById('button');
+let glass = document.getElementById('food-search');
 
-btn.addEventListener('click', function(){
+let heart = document.getElementById('heart-symbol');
+
+
+glass.addEventListener('click', function(){
+    let inputtext = document.getElementById('searchbar').value;
+
+    console.log(inputtext);
     let xhr = new XMLHttpRequest();
 
-    xhr.open('get', 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata' );
+    xhr.open('get', `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputtext}`, true );
     xhr.send();
     xhr.onload = function(){
+        console.log('https://www.themealdb.com/api/json/v1/1/search.php?s='+ inputtext);
         console.log(xhr.response);
         var jsonresponse = JSON.parse(xhr.response);
         imgs.setAttribute('src',jsonresponse.meals[0].strMealThumb ) ;
         
+    }
+});
 
+document.getElementById('heart').addEventListener('click', function(){
+    if(heart.style.color == 'black' ){
+        heart.style.color = 'red';
+    }else{
+        heart.style.color = 'black';
     }
 })
 
